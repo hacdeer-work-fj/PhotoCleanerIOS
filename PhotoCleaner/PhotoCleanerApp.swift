38182f19df04c6f73ac84479e81872198bd42f18
hacdeer-work-fj <267559805+hacdeer-work-fj@users.noreply.github.com>
2026-07-08 01:19:54 +0800

    Improve thumbnail taps and clean GIF cache

--- PhotoCleaner/PhotoCleanerApp.swift
+++ PhotoCleaner/PhotoCleanerApp.swift
@@ -2,6 +2,10 @@ import SwiftUI
 
 @main
 struct PhotoCleanerApp: App {
+    init() {
+        TemporaryCacheManager.cleanIfNeeded()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
